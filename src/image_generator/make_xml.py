e0f4f9bfde21f5db880f9fc34d6ea7778834e95f
hyeeji <gpwl7597@jbnu.ac.kr>
2022-03-18 16:25:16 +0900

    test : excel_

---
+++ src/image_generator/make_xml.py
@@ -0,0 +1,13 @@
+import openpyxl
+
+data = openpyxl.load_workbook("C:/Users/hyejiHan/Downloads/svg_database+pattern_code_ver3.xlsx", data_only=True)
+
+base = data['base']
+sticker = data['sticker']
+
+
+
+
+
+
+
