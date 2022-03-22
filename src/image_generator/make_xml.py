52804f47fde8e4db2932edab13a13e7953a26844
Han Hyeji <77146030+Hyeeji@users.noreply.github.com>
2022-03-22 17:03:49 +0900

    Test:Make xml

--- src/image_generator/make_xml.py
+++ src/image_generator/make_xml.py
@@ -1,9 +1,26 @@
 import openpyxl
+from xml.etree.ElementTree import Element, SubElement, ElementTree
 
 data = openpyxl.load_workbook("C:/Users/hyejiHan/Downloads/svg_database+pattern_code_ver3.xlsx", data_only=True)
 
 base = data['base']
 sticker = data['sticker']
+
+if __name__ == '__main__':
+    root = Element('mappingdata')
+
+    pattern = SubElement(root, 'pattern')
+
+    '''SubElement(pattern, 'svgPath').text = svgPath
+    SubElement(pattern, 'dxfPath').text = dxfPath'''
+
+    base = SubElement(pattern, 'base')
+
+    '''SubElement(base, 'category').text = category
+    SubElement(base, 'feature').text = feature
+    SubElement(base, 'block').text = block'''
+
+    #SubElement()
 
 
 
