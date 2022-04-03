61bd0344e7942e6c77b7c47f328d21f1e9f4cd17
Han Hyeji <77146030+Hyeeji@users.noreply.github.com>
2022-04-04 02:06:57 +0900

    Resize png

--- src/image_generator/svg_to_png.py
+++ src/image_generator/svg_to_png.py
@@ -2,6 +2,8 @@ import os
 from pathlib import Path
 from cairosvg import svg2png
 import xml.etree.ElementTree as ET
+import numpy as np
+import cv2
 
 
 def svg_to_png(src, dest, width, height):
@@ -24,9 +26,65 @@ def remove_back_svg(svg_path):
             if "B" in tag_name:
                 first_level.remove(second_level)
 
-    tree.write(src_svg)
+    tree.write(svg_path)
 
 
+def resize_svg(svg_path):
+    ET.register_namespace('', "http://www.w3.org/2000/svg")  # should add namespace
+    tree = ET.parse(svg_path)
+    root = tree.getroot()
+
+    size = root.attrib['viewBox']
+    size = size.split(' ')
+
+    height = float(size[2]) / 2
+
+    size = size[0] + " " + size[1] + " " + str(height) + " " + size[3]
+
+    root.attrib['viewBox'] = size
+
+    tree.write(svg_path)
+
+
+def resize_png(png_path):
+
+    img = cv2.imread(png_path, cv2.IMREAD_UNCHANGED)
+
+    bw_img = (img[:, :, 3] > 0)  # alhpa != 0
+
+    img_width = bw_img.shape[1]
+
+    last_point = 0
+    current_w = 0
+
+    points = []
+
+    first = True
+
+    while current_w < img_width:
+        row_arr = bw_img[current_w, :]
+        positive_indices = np.where(row_arr)  # for each column of pixels
+
+        if (current_w - last_point) > 1:
+            break
+
+        if len(positive_indices[0]) == 0:  # if there is no meaningful pixel, continue
+            current_w += 1
+            if len(points) == 0:
+                last_point = current_w
+            continue
+
+        if first:
+            first = False
+            points.append(current_w)
+
+        # for every width_stride column
+        current_w += 1
+        last_point = current_w
+
+    points.append(last_point)
+    
+    print(points)
 
 
 if __name__ == '__main__':
@@ -55,4 +113,7 @@ if __name__ == '__main__':
                     dest_png = str(png_path)+'\\'+svg
                     dest_png = dest_png.replace('.svg', '.png')
 
+                    resize_svg(src_svg)
                     svg_to_png(src=src_svg, dest=dest_png, width=width, height=height)
+                    resize_png(dest_png)
+
